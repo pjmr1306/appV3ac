81753e631cb4bccf89a3dfc495aa281869ae6d23
PM13 <pjmr1306@gmail.com>
2018-08-20 15:46:31 -0300

    Creaciondel menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatMenuModule, MatMenu, MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule,  MatToolbarModule,
+  MatSidenavModule, MatIconModule, MatListModule,
+  MatGridListModule, MatCardModule, MatMenuModule} from '@angular/material';
 import { MaterialModule } from './material/material.module';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialDemoComponent } from './material-demo/material-demo.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [MaterialDemoComponent]
 })
 export class MaterialModule { }
